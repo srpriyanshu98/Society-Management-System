771d271d0e9c0dace19a18e77a8b7be6eb4e0885
bhargav107 <bhargav.dankhara@swiftrut.com>
2024-10-24 22:35:49 +0530

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,6 +4,7 @@ import { ENV_VARS } from "./envVars.js";
 export const connectDB = async () => {
 	try {
 		const conn = await mongoose.connect(ENV_VARS.MONGO_URI);
+		console.log("Connected to database: ", conn.connection.host);
 	} catch (error) {
 		console.error("Error while connecting to database: ", error.message);
 		process.exit(1);
--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const ENV_VARS = {
+	MONGO_URI: process.env.MONGO_URI,
 	PORT: process.env.PORT || 5000,
 
 	NODE_ENV: process.env.NODE_ENV || "development",
