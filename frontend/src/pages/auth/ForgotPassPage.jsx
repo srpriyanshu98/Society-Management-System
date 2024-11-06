b40e16d8f9b6c13b0e9a17cc3c1b3e180e598037
bhargav107 <bhargav.dankhara@swiftrut.com>
2024-11-07 02:20:06 +0530

    class name bug

--- frontend/src/pages/auth/ForgotPassPage.jsx
+++ frontend/src/pages/auth/ForgotPassPage.jsx
@@ -4,39 +4,36 @@ export default function ForgotPassPage() {
 	// States for the process
 
 	return (
-		
 		<div className="min-h-screen flex flex-col md:flex-row items-center relative">
 			{/* Left side - Image */}
 			<div className="w-full md:w-1/2 bg-transparent md:bg-[#F6F8FB] h-screen flex items-center justify-center p-4 relative">
-			<img
-				src="./src/assets/Bright Web.png"
-				alt="Illustration"
-				className="absolute top-14 left-40"
-			/>
-			{/* Left side - Image */}
-			<div className="hidden md:block m-auto ">
 				<img
-					src="./src/assets/forgot.png"
+					src="./src/assets/Bright Web.png"
 					alt="Illustration"
-					className="z-10"
+					className="absolute top-14 left-40"
 				/>
-				<img
-					src="./src/assets/bg-graphic.png"
-					alt=""
-					className="absolute top-0 right-0 h-screen -z-10"
-				/>
+				{/* Left side - Image */}
+				<div className="hidden md:block m-auto ">
+					<img
+						src="./src/assets/forgot.png"
+						alt="Illustration"
+						className="z-10"
+					/>
+					<img
+						src="./src/assets/bg-graphic.png"
+						alt=""
+						className="absolute top-0 right-0 h-screen -z-10"
+					/>
+				</div>
 			</div>
-			</div>
-	  
 			{/* Right side - Form */}
-			
-				<img
-					src="./src/assets/bg-graphic.png"
-					alt=""
-					className="absolute top-0 right-0 h-full md:h-screen -z-10"
-				/>
-				<ResetPassword />
-			
+
+			<img
+				src="./src/assets/bg-graphic.png"
+				alt=""
+				className="absolute top-0 right-0 h-full md:h-screen -z-10"
+			/>
+			<ResetPassword />
 		</div>
 	);
 }
