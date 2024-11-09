1a4d3c9d2dfb4140c2a94760d037a99f18c37a29
vishal-157 <vishal.savaliya@swiftrut.com>
2024-11-10 01:00:14 +0530

    create security api and add role authentication

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -4,18 +4,17 @@ import { ENV_VARS } from "../config/envVars.js";
 export const sendMail = async (to, subject, text) => {
   const transporter = nodemailer.createTransport({
     host: ENV_VARS.EMAIL_HOST,
-    port: ENV_VARS.EMAIL_PORT,
+    secure: Number(ENV_VARS.EMAIL_PORT) === 465, // Use secure for port 465
     auth: {
       user: ENV_VARS.EMAIL_USER,
       pass: ENV_VARS.EMAIL_PASSWORD,
     },
-    secure: ENV_VARS.EMAIL_PORT == 465, // Use true for port 465, false for port 587
   });
   const mailOptions = {
     from: ENV_VARS.EMAIL_USER,
-    to,
-    subject,
-    text,
+    to: to,
+    subject: subject,
+    text: text,
   };
   await transporter.sendMail(mailOptions);
 };
--- backend/middleware/authmiddleware.js
+++ backend/middleware/authmiddleware.js
@@ -1,4 +1,6 @@
 import jwt from "jsonwebtoken";
+import User from "../model/usermodel.js";
+import { ENV_VARS } from "../config/envVars.js";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.token;
@@ -9,4 +11,34 @@ export const verifyToken = (req, res, next) => {
     req.userId = decoded.id;
     next();
   });
+};
+
+export const admin = async (req, res, next) => {
+  try {
+    // Check for token in headers
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "No token provided, authorization denied" });
+    }
+
+    // Verify token
+    const decoded = jwt.verify(token, ENV_VARS.JWT_SECRET);
+    req.user = await User.findById(decoded.id);
+
+    // Check if user exists and has an admin role
+    if (!req.user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    if (req.user.role !== "admin") {
+      return res.status(403).json({ message: "Access denied, admin only" });
+    }
+
+    next(); // User is an admin, continue to the next middleware
+  } catch (error) {
+    res
+      .status(401)
+      .json({ message: "Token verification failed", error: error.message });
+  }
 };
