73c4b2fe02aed6ed1397adfb2d39e6c6750d29bf
sr-rohit-158 <rohit.dhori@swiftrut.com>
2024-11-13 06:07:54 +0530

    Visitor Logs and small task

--- frontend/src/pages/admin/SecurityManagement.jsx
+++ frontend/src/pages/admin/SecurityManagement.jsx
@@ -1,16 +1,13 @@
 import Layout from "@/components/Layout";
+import ScurityManagementlogs from "@/components/securitymanagement/scuritymanagementloge";
+
 
 export default function SecurityManagement({ userRole }) {
 	return (
 		<Layout userRole={userRole}>
-			<div className="flex flex-col p-6  bg-gray-50 h-screen overflow-auto">
-				<h1 className="text-2xl font-bold">
-					Welcome to the Security Management
-				</h1>
-				<p>This is the main workspace area.</p>
-				{/* Other components or routes can be rendered here */}
+			<div className="flex flex-col h-full overflow-auto">
 				<div>
-					<img src="./src/assets/1.png" alt="" />
+					<ScurityManagementlogs />
 				</div>
 			</div>
 		</Layout>
