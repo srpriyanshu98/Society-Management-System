d8457a9939bf8f330e61955628b306a8626a5878
srpriyanshu98 <priyanshu.mishra@swiftrut.com>
2024-11-27 05:20:25 +0530

    add password validation

--- backend/routes/authroutes.js
+++ backend/routes/authroutes.js
@@ -7,13 +7,15 @@ import {
 	resetPassword,
 	updateProfile,
 	getUserProfile,
+	validatePassword,
 } from "../controller/authcontroller.js";
-
+import { verifyToken } from "../middleware/authmiddleware.js";
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
+router.post("/validate-password", verifyToken, validatePassword);
 
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
