d0389c678c3889d4b90ad232fd773d46bc6a6d7b
bhargav107 <bhargav.dankhara@swiftrut.com>
2024-12-07 23:04:16 +0530

    build comm

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,18 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            // eslint-disable-next-line no-undef
-            "@": path.resolve(__dirname, "./src"),
-        },
-    },
-    server: {
-        port: 3000,
-    },
+	plugins: [react()],
+	build: {
+		outDir: "dist", // Output directory
+		emptyOutDir: true,
+	},
+	resolve: {
+		alias: {
+			// eslint-disable-next-line no-undef
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	server: {
+		port: 3000,
+	},
 });
