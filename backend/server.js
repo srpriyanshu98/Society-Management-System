a1d4eed6cf0ead938ee9e0cc25a63030cfc1e541
bhargav107 <bhargav.dankhara@swiftrut.com>
2024-12-08 01:55:21 +0530

    url update

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ const PORT = ENV_VARS.PORT;
 
 // Allow requests from your frontend domain
 const corsOptions = {
-	origin: "society-management-system-omega.vercel.app",
+	origin: "https://society-management-system-omega.vercel.app",
 	optionsSuccessStatus: 200,
 };
 
