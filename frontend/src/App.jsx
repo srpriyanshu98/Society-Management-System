04e2c3af17810026a206c6dc4564b8efec805601
bhargav107 <bhargav.dankhara@swiftrut.com>
2024-12-07 23:59:43 +0530

    deplouments error resolve

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,56 +1,94 @@
 import { Route, Routes } from "react-router-dom";
-
-// AUTH IMPORTS
 import Signup from "./pages/auth/SignUpPage";
+import Login from "./pages/auth/LogInPage";
 import ForgotPassPage from "./pages/auth/ForgotPassPage";
 import EditProfile from "./pages/EditProfile";
-import Login from "./pages/auth/LogInPage";
-
-// TEST IMPORTS
-import Slider from "./test/Slider";
-import Sellect from "./test/Sellect";
-
-// ADMIN IMPORTS
+import ProtectedRoute from "./middlewares/ProtectedRoute";
 import Dashboard from "./pages/admin/Dashboard";
 import ResidentManagement from "./pages/admin/ResidentManagement";
-import FacilityManagement from "./pages/admin/FacilityManagement";
-import ComplaintTracking from "./pages/admin/ComplaintTracking";
-import SecurityManagement from "./pages/admin/SecurityManagement";
-import SecurityGuard from "./pages/admin/SecurityGuard";
-import Announcement from "./pages/admin/Announcement";
 import ResidentForm from "./components/residentManagement/ResidentForm";
+import FinancialManagement from "./pages/admin/FinancialManagement";
+import ViewOtherIncome from "./components/financialManagement/Viewotherincome";
 import AddExpenses from "./pages/admin/AddExpenses";
 import Note from "./pages/admin/Note";
+import FacilityManagement from "./pages/admin/FacilityManagement";
+import ComplaintTracking from "./pages/admin/ComplaintTracking";
 import RequestTracking from "./pages/admin/RequestTracking";
+import SecurityManagement from "./pages/admin/SecurityManagement";
 import SecurityRuls from "./pages/admin/SecurityRuls";
-
-// RESIDENT IMPORTS
+import SecurityGuard from "./pages/admin/SecurityGuard";
+import Announcement from "./pages/admin/Announcement";
 import PersonalDetail from "./pages/resident/PersonalDetail";
 import ServiceAndComplaint from "./pages/resident/ServiceAndComplaint";
 import EventsParticipation from "./pages/resident/EventsParticipation";
 import Community from "./pages/resident/Community";
+import ChatCard from "./components/Resident-Components/Community/AccessForums/ChatCard";
+import ChatHeader from "./components/Resident-Components/Community/AccessForums/ChatHeader";
+import ChatTextArray from "./components/Resident-Components/Community/AccessForums/ChatTextArray";
+import ChatFooter from "./components/Resident-Components/Community/AccessForums/ChatFooter";
 import Polls from "./pages/resident/Polls";
 import CommunitiesDiscussion from "./pages/resident/CommunitiesDiscussion";
 import MaintenanceInvoices from "./pages/resident/MaintenanceInvoices";
 import OtherInvoice from "./pages/resident/OtherInvoice";
 import SecurityProtocols from "./pages/resident/SecurityProtocols";
-
-// SECURITY IMPORTS
-import VisitortrackingScreen from "./pages/security/visitortrackingscreen";
-import EmergencyManagement from "./pages/security/emergencymanagement";
-
-// COMPONENTS IMPORTS
-import ChatCard from "./components/Resident-Components/Community/AccessForums/ChatCard";
-import ChatHeader from "./components/Resident-Components/Community/AccessForums/ChatHeader";
-import ChatTextArray from "./components/Resident-Components/Community/AccessForums/ChatTextArray";
-import ChatFooter from "./components/Resident-Components/Community/AccessForums/ChatFooter";
-
-// MIDDLEWARES IMPORTS
-import ProtectedRoute from "./middlewares/ProtectedRoute";
 import PaymentPage from "./pages/PaymentPage";
 import SuccessPage from "./pages/SuccessPage";
-import ViewOtherIncome from "./components/financialManagement/ViewOtherIncome";
-import FinancialManagement from "./pages/admin/FinancialManagement";
+import VisitortrackingScreen from "./pages/security/visitortrackingscreen";
+import EmergencyManagement from "./pages/security/emergencymanagement";
+import Slider from "./test/Slider";
+import Sellect from "./test/Sellect";
+
+// AUTH IMPORTS
+// import Signup from "./pages/auth/SignUpPage";
+// import ForgotPassPage from "./pages/auth/ForgotPassPage";
+// import EditProfile from "./pages/EditProfile";
+// import Login from "./pages/auth/LogInPage";
+
+// // TEST IMPORTS
+// import Slider from "./test/Slider";
+// import Sellect from "./test/Sellect";
+
+// // ADMIN IMPORTS
+// import Dashboard from "./pages/admin/Dashboard";
+// import ResidentManagement from "./pages/admin/ResidentManagement";
+// import FacilityManagement from "./pages/admin/FacilityManagement";
+// import ComplaintTracking from "./pages/admin/ComplaintTracking";
+// import SecurityManagement from "./pages/admin/SecurityManagement";
+// import SecurityGuard from "./pages/admin/SecurityGuard";
+// import Announcement from "./pages/admin/Announcement";
+// import ResidentForm from "./components/residentManagement/ResidentForm";
+// import AddExpenses from "./pages/admin/AddExpenses";
+// import Note from "./pages/admin/Note";
+// import RequestTracking from "./pages/admin/RequestTracking";
+// import SecurityRuls from "./pages/admin/SecurityRuls";
+
+// // RESIDENT IMPORTS
+// import PersonalDetail from "./pages/resident/PersonalDetail";
+// import ServiceAndComplaint from "./pages/resident/ServiceAndComplaint";
+// import EventsParticipation from "./pages/resident/EventsParticipation";
+// import Community from "./pages/resident/Community";
+// import Polls from "./pages/resident/Polls";
+// import CommunitiesDiscussion from "./pages/resident/CommunitiesDiscussion";
+// import MaintenanceInvoices from "./pages/resident/MaintenanceInvoices";
+// import OtherInvoice from "./pages/resident/OtherInvoice";
+// import SecurityProtocols from "./pages/resident/SecurityProtocols";
+
+// // SECURITY IMPORTS
+// import VisitortrackingScreen from "./pages/security/visitortrackingscreen";
+// import EmergencyManagement from "./pages/security/emergencymanagement";
+
+// // COMPONENTS IMPORTS
+// import ChatCard from "./components/Resident-Components/Community/AccessForums/ChatCard";
+// import ChatHeader from "./components/Resident-Components/Community/AccessForums/ChatHeader";
+// import ChatTextArray from "./components/Resident-Components/Community/AccessForums/ChatTextArray";
+// import ChatFooter from "./components/Resident-Components/Community/AccessForums/ChatFooter";
+
+// // MIDDLEWARES IMPORTS
+// import ProtectedRoute from "./middlewares/ProtectedRoute";
+// import PaymentPage from "./pages/PaymentPage";
+// import SuccessPage from "./pages/SuccessPage";
+// import ViewOtherIncome from "./components/financialManagement/ViewOtherIncome";
+// import FinancialManagement from "./pages/admin/FinancialManagement";
 
 function App() {
 	return (
