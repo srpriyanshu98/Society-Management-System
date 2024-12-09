341f00a7704af54d07b96d8c2d18285d791ec957
bhargav107 <bhargav.dankhara@swiftrut.com>
2024-12-10 00:03:42 +0530

    removing comp to check if img deployes in versal

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,14 +1,14 @@
-import CompressionPlugin from "compression-webpack-plugin";
-import CopyWebpackPlugin from "copy-webpack-plugin";
+// import CompressionPlugin from "compression-webpack-plugin";
+// import CopyWebpackPlugin from "copy-webpack-plugin";
 
-export default {
-	plugins: [
-		new CompressionPlugin({
-			algorithm: "gzip",
-			test: /\.(js|css|html)$/,
-		}),
-		new CopyWebpackPlugin({
-			patterns: [{ from: "src/assets", to: "assets" }],
-		}),
-	],
-};
+// export default {
+// 	plugins: [
+// 		new CompressionPlugin({
+// 			algorithm: "gzip",
+// 			test: /\.(js|css|html)$/,
+// 		}),
+// 		new CopyWebpackPlugin({
+// 			patterns: [{ from: "src/assets", to: "assets" }],
+// 		}),
+// 	],
+// };
