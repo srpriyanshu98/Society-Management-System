8c12d9cca38c64de25637b5fb19cffda380588fd
bhargav107 <bhargav.dankhara@swiftrut.com>
2024-12-09 22:44:05 +0530

    public img for deployment

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,10 +1,14 @@
 import CompressionPlugin from "compression-webpack-plugin";
+import CopyWebpackPlugin from "copy-webpack-plugin";
 
 export default {
 	plugins: [
 		new CompressionPlugin({
 			algorithm: "gzip",
-			test: /\.(js|css|html|svg)$/,
+			test: /\.(js|css|html)$/,
+		}),
+		new CopyWebpackPlugin({
+			patterns: [{ from: "src/assets", to: "assets" }],
 		}),
 	],
 };
